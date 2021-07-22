45a5303c1a7602f9b3a12daebe21c3229408ecfe
nightlaro <mickocabacungan@gmail.com>
2021-07-22 13:58:32 -0700

    Revert back refactor to fix null pointer exception

--- app/src/main/java/com/nightlaro/listmaker/DetailActivity.kt
+++ app/src/main/java/com/nightlaro/listmaker/DetailActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class DetailActivity : AppCompatActivity() {
+
+    lateinit var list : Tasklist
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        list = intent.getParcelableExtra(MainActivity.INTENT_LIST_KEY)!!
+        title = list.name
     }
 }
