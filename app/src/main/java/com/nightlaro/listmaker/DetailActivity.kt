0700d0484887b8ccb26f1bb552adc1e0ae54f429
vaudevillain <jjshin85@gmail.com>
2021-07-28 12:09:56 -0700

    fix up adapter. add scratch class to talk inheritence

--- app/src/main/java/com/nightlaro/listmaker/DetailActivity.kt
+++ app/src/main/java/com/nightlaro/listmaker/DetailActivity.kt
@@ -23,29 +23,27 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
-    private lateinit var list : Tasklist
-    private lateinit var taskListRecyclerView: RecyclerView
-    private lateinit var addTaskButton: FloatingActionButton
+    private lateinit var taskListAdapter: TaskListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
-        list = intent.getParcelableExtra(MainActivity.INTENT_LIST_KEY)!!
+        val list: Tasklist = intent.getParcelableExtra(MainActivity.INTENT_LIST_KEY)!!
         title = list.name
 
-        taskListRecyclerView = findViewById(R.id.task_list_recyclerview)
+        val taskListRecyclerView = findViewById<RecyclerView>(R.id.task_list_recyclerview)
         taskListRecyclerView.layoutManager = LinearLayoutManager(this)
-        taskListRecyclerView.adapter = TaskListAdapter(list)
-        addTaskButton = findViewById(R.id.add_task_button)
+        taskListAdapter = TaskListAdapter(list)
+        taskListRecyclerView.adapter = taskListAdapter
+        val addTaskButton = findViewById<FloatingActionButton>(R.id.add_task_button)
         addTaskButton.setOnClickListener {
             showCreateTaskDialog()
         }
     }
 
     override fun onBackPressed() {
-        Log.d("OnBackPressed", "LIST: $list")
         val bundle = Bundle() //"A bundle is for storing things" - Tutorial
-        bundle.putParcelable(MainActivity.INTENT_LIST_KEY, list)
+        bundle.putParcelable(MainActivity.INTENT_LIST_KEY, taskListAdapter.list)
         val intent = Intent()
         intent.putExtras(bundle)
         setResult(Activity.RESULT_OK, intent)
@@ -60,10 +58,10 @@ class DetailActivity : AppCompatActivity() {
             .setView(taskEditText)
             .setPositiveButton(R.string.add_task) { dialog, _ ->
                 val task = taskEditText.text.toString()
-                val oldList = list
-                list = oldList.copy(tasks = oldList.tasks + task)
-                taskListRecyclerView.adapter = TaskListAdapter(list) //Yeah... :c
-                Log.d("ADD_TASK", "Current task list: ${list.tasks}")
+                val oldList = taskListAdapter.list
+                val newList = oldList.copy(tasks = oldList.tasks + task)
+                taskListAdapter.list = newList
+                Log.d("ADD_TASK", "Current task list: ${taskListAdapter.list.tasks}")
                 dialog.dismiss()
             }
             .create()
--- app/src/main/java/com/nightlaro/listmaker/TaskListAdapter.kt
+++ app/src/main/java/com/nightlaro/listmaker/TaskListAdapter.kt
@@ -5,10 +5,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.lang.IllegalArgumentException
 
-class TaskListAdapter(var list : Tasklist): RecyclerView.Adapter<TaskListAdapter.TaskListViewHolder>() {
+class TaskListAdapter(initialList: Tasklist) : RecyclerView.Adapter<TaskListAdapter.TaskListViewHolder>() {
+    var list: Tasklist = initialList
+        set(newValue) {
+            // example things you can do in a setter
+
+            // check for equality. If nothing's changed, don't execute more code (such as
+            // calling "notifyDataSetChanged()"
+            if (newValue == field) return
+
+            // Check new value is valid
+            if (newValue.name.isBlank()) throw IllegalArgumentException("Name was blank")
+
+            // update backing field of the property
+            field = newValue
+
+            notifyDataSetChanged()
+        }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskListViewHolder {
-
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.task_view_holder, parent, false)
         return TaskListViewHolder(view)
@@ -21,6 +38,7 @@ class TaskListAdapter(var list : Tasklist): RecyclerView.Adapter<TaskListAdapter
     override fun getItemCount(): Int {
         return list.tasks.size
     }
+
     class TaskListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val taskListTextView = itemView.findViewById<TextView>(R.id.textview_task)
     }
